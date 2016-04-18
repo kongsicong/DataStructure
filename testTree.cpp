f7476f22c0414268dbe24cc3cd8bb2e352b45908
kongsicong <1390159970@qq.com>
2016-04-18 11:18:45 +0800

    the test file for the tree

---
+++ testTree.cpp
@@ -0,0 +1,27 @@
+﻿#pragma once
+#pragma execution_character_set("utf-8")
+
+#include "testTree.h"
+#include <stdio.h>
+void testTree() {
+	Status(*Visit)(TElemType e) = &PrintTElemType;
+	BiTree T;
+	CreateBiTree(T);
+	printf("traverse binary tree with preorder:\n");
+	PreOrderTraverse(T, Visit);
+	printf("\n");
+
+	printf("traverse binary tree with inorder:\n");
+	InOrderTraverse(T, Visit);
+	printf("\n");
+
+	printf("traverse binary tree with postorder:\n");
+	PostOrderTraverse(T, Visit);
+	printf("\n");
+}
+
+
+Status PrintTElemType(TElemType e) {
+	printf("%c", e);
+	return OK;
+}
---
+++ testTree.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+#pragma execution_character_set("utf-8")
+
+#include "tree/tree.h"
+using namespace tree;
+
+void testTree();
+
+int PrintTElemType(TElemType e);
