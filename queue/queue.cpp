a03e5f8d5ebc0b819db6622c0e07b1b02f9bb4b5
kongsicong <1390159970@qq.com>
2016-04-18 11:15:27 +0800

    modify the queue

--- queue/queue.cpp
+++ queue/queue.cpp
@@ -1,1 +1,122 @@
 #include "queue.h"
+#include <stdlib.h>
+namespace queue {
+	Status InitQueue(LinkQueue &Q) {
+		Q.front = Q.rear = (QNode*)malloc(sizeof(QNode));
+		if (!Q.front) exit(OVERFLOW);
+		Q.front->next = nullptr;
+		return OK;
+	}
+
+	Status DestroyQueue(LinkQueue &Q) {
+		while (Q.front) {
+			Q.rear = Q.front->next;
+			free(Q.front);
+			Q.front = Q.rear;
+		}
+		return OK;
+	}
+
+	Status ClearQueue(LinkQueue &Q) {
+		QNode * cur = Q.front->next;
+		while (cur) {
+			Q.rear = cur->next;
+			free(cur);
+			cur = Q.rear;
+		}
+		Q.rear = Q.front;
+		Q.front->next = nullptr;
+		return OK;
+	}
+
+	Status QueueEmpty(LinkQueue Q) {
+		if (Q.front == Q.rear) return TRUE;
+		else return FALSE;
+	}
+
+	int QueueLength(LinkQueue Q) {
+		int i = 0;
+		for (QNode* cur = Q.front->next; cur != nullptr; cur = cur->next, i++);
+		return i;
+	}
+
+	Status EnQueue(LinkQueue &Q, QElemType e) {
+		QNode *node = (QNode*)malloc(sizeof(QNode));
+		if (!node) return OVERFLOW;
+		node->data = e;
+		node->next = nullptr;
+		Q.rear->next = node;
+		Q.rear = node;
+		return OK;
+	}
+
+	Status DeQueue(LinkQueue &Q, QElemType &e) {
+		if (Q.front == Q.rear) return OVERFLOW;
+		
+		e = Q.front->next->data;
+		if (Q.front->next == Q.rear) {
+			free(Q.front->next);
+			Q.front->next = nullptr;
+			Q.rear = Q.front;
+		}
+		else {
+			QNode* cur = Q.front->next;
+			Q.front->next = cur->next;
+			free(cur);
+		}
+		return OK;
+
+	}
+
+	Status GetHead(LinkQueue Q, QElemType &e) {
+		if (Q.rear == Q.front) return ERROR;
+		e = Q.front->next->data;
+		return OK;
+	}
+
+
+
+
+
+
+
+
+
+
+	/*********************************************************************************************************************************************************/
+	Status InitQueue(SqQueue &Q) {
+		Q.base = (QElemType*)malloc(MAXQSIZE * sizeof(QElemType));
+		if (!Q.base) return OVERFLOW;
+		Q.front = Q.rear = 0;
+		return OK;
+	}
+	Status DestroyQueue(SqQueue &Q) {
+		free(Q.base);
+		Q.base = nullptr;
+		Q.front = Q.rear = 0;
+		return OK;
+	}
+	Status ClearQueue(SqQueue &Q) {
+		Q.front = Q.rear = 0;
+		return OK;
+	}
+	Status QueueEmpty(SqQueue Q) {
+		if (Q.front == Q.rear) return TRUE;
+		else return FALSE;
+	}
+	int QueueLength(SqQueue Q) {
+		return (Q.rear - Q.front + MAXQSIZE) % MAXQSIZE;
+	}
+	Status EnQueue(SqQueue &Q, QElemType e) {
+		if ((Q.rear + 1) % MAXQSIZE == Q.front) return ERROR;
+		Q.base[Q.rear] = e;
+		Q.rear = (Q.rear + 1) % MAXQSIZE;
+		return OK;
+	}
+	Status DeQueue(SqQueue &Q, QElemType &e) {
+		if (QueueEmpty(Q)) return ERROR;
+		e = Q.base[Q.front];
+		Q.front = (Q.front + 1) % MAXQSIZE;
+		return OK;
+	}
+}
--- queue/queue.h
+++ queue/queue.h
@@ -1,4 +1,49 @@
 #ifndef _QUEUE_H_
 #define _QUEUE_H_
+namespace queue {
+#define TRUE 1
+#define FALSE 0
+#define OK 1
+#define ERROR 0
+#define INFEASIBLE -1
+#define OVERFLOW -2
 
+	typedef int Status;
+	typedef int  QElemType;
+	typedef struct QNode {
+		QElemType data;
+		struct QNode *next;
+	}QNode;
+	typedef struct {
+		QNode *front;
+		QNode *rear;
+	}LinkQueue;
+
+	Status InitQueue(LinkQueue &Q);
+	Status DestroyQueue(LinkQueue &Q);
+	Status ClearQueue(LinkQueue &Q);
+	Status QueueEmpty(LinkQueue Q);
+	int QueueLength(LinkQueue Q);
+	Status GetHead(LinkQueue Q, QElemType &e);
+	Status EnQueue(LinkQueue &Q, QElemType e);
+	Status DeQueue(LinkQueue &Q, QElemType &e);
+	//Status QueueTraverse(LinkQueue Q, visit());
+
+
+#define MAXQSIZE 100
+	typedef struct {
+		QElemType *base;
+		int front;
+		int rear;
+	}SqQueue;
+	
+	Status InitQueue(SqQueue &Q);
+	Status DestroyQueue(SqQueue &Q);
+	Status ClearQueue(SqQueue &Q);
+	Status QueueEmpty(SqQueue Q);
+	int QueueLength(SqQueue Q);
+	Status GetHead(SqQueue Q, QElemType &e);
+	Status EnQueue(SqQueue &Q, QElemType e);
+	Status DeQueue(SqQueue &Q, QElemType &e);
+}
 #endif //_QUEUE_H_
