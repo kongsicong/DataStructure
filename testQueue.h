dc1aead67740717dc528506b195ce6c2eb6d3ffc
kongsicong <1390159970@qq.com>
2016-04-18 11:19:50 +0800

    the test file for the queue

--- testQueue.h
+++ testQueue.h
@@ -1,4 +1,14 @@
 #ifndef _TEST_QUEUE_H_
 #define _TEST_QUEUE_H_
+#include "queue/queue.h"
+using namespace queue;
 void testQueue();
+void testQueueWithList();
+void testQueueWithArray();
+
+void PrintQueue(LinkQueue Q);
+void CreateQueue(LinkQueue &Q);
+
+void PrintQueue(SqQueue Q);
+void CreateQueue(SqQueue &Q);
 #endif //_TEST_QUEUE_H_
